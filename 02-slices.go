4ff823d1385704ae9feae1b47ad22a0988cf78e4
dcrod <dcrod@users.noreply.github.com>
2020-09-19 21:21:13 -0700

    Update picture patterns and formatting

--- 02-slices.go
+++ 02-slices.go
@@ -9,7 +9,7 @@ func pattern(x, y, i int) uint8 {
 	case 1:
 		return uint8(x * y)
 	case 2:
-		return uint8(x^y)
+		return uint8(x^(4*y)*x)
 	case 3:
 		return uint8(y^x * x^(3*y))
 	default:
@@ -18,16 +18,13 @@ func pattern(x, y, i int) uint8 {
 }
 
 func Pic(dx, dy int) [][]uint8 {
-
 	pic := make([][]uint8, dy)
-
 	for y := range pic {
 		pic[y] = make([]uint8, dx)
 		for x := range pic[y] {
-			pic[y][x] = pattern(x, y, 3)
+			pic[y][x] = pattern(x, y, 2)
 		}
 	}
-
 	return pic
 }
 
